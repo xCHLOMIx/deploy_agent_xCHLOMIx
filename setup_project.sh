487f9fb950ad1b5fa889deb71330484dea8899bc
xCHLOMIx <gutabarwaa@gmail.com>
2026-06-16 19:32:46 +0200

    Add function setup_directory in setup_project.sh

--- setup_project.sh
+++ setup_project.sh
@@ -1,1 +1,19 @@
 #!/usr/bin/env bash
+
+read -p "Enter tracker version (e.g: v1): " version
+
+directory_name="attendance_tracker_$version"
+
+function setup_directory() {
+	echo "Setting up directory structure..."
+	mkdir "./$directory_name" "./$directory_name/Helpers" "./$directory_name/reports"
+	echo "Directory structure successfully setup!"
+	echo "Adding initial files..."
+	cp files/attendance_checker.py "./$directory_name"
+	cp files/assets.csv "./$directory_name/Helpers"
+	cp files/config.json "./$directory_name/Helpers"
+	cp files/reports.log "./$directory_name/reports"
+	echo "Initial files successfully added!"
+}
+
+setup_directory
